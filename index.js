7f8d5d247dbc02d9043501e1dcd18c9548aa6ee9
thais <thaisster@gmail.com>
2023-03-07 14:36:23 -0300

    projeto6

---
+++ index.js
@@ -0,0 +1,23 @@
+function showPassword() {
+    const eye = document.getElementById('eye');
+    const eyeSlash = document.getElementById ('eye-slash');
+    const fielPassword = document.getElementById('field-password');
+
+    if(eye.style.display ==='none'){
+
+        eye.style.display = 'block';
+        eyeSlash.style.display = 'none';
+        fielPassword.type = 'text';
+        
+    } else{
+        eye.style.display = 'none';
+        eyeSlash.style.display = 'block';
+        fielPassword.type = 'password';
+
+    }
+};
+
+document.getElementById('btn-login').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('logado');
+});
